e29eaaf777263a0b36eff3640072332ff9f0ae10
Christian Schmitz <csc@shd.de>
2019-08-13 15:46:20 +0200

    Initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+D
+src/tbCom_Hello.java,7\a\7af5b39843f6562e5cc5ebff4a035d2579a31639
+U
+%src/testing_board/TestCaseChrome.java,e\2\e2aa029dbca6bfd091c5dc11e05bb87ec41e345b
---
+++ src/tbCom_Hello.java
@@ -0,0 +1,7 @@
+public class tbCom_Hello {
+    public static void main(String[] args) {
+
+        String string_hello = "Hello Testing-Board Reader!";
+        System.out.println(string_hello);
+    }
+}
